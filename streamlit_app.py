c45485ec7d6483708745196ea9d6f3b71b4abce1
Bamidele Ajamu <54434309+BamideleAdeola@users.noreply.github.com>
2024-10-07 06:34:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 # Import python packages
 import streamlit as st
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+
 # from snowflake.snowpark.context import get_active_session
 
 # Write directly to the app
